8e5d2adcdc0a57aadb676238f52070c4263e01ac
faraznaeem <faraznaeem@gmail.com>
2019-10-27 07:09:28 +0100

    Adds axios and sets up youtube api

---
+++ src/apis/youtube.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+const KEY = "AIzaSyBkmtDrIQRwoVNNXWnb_FKZfXeHKuYSgms";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: { part: "snippet", maxResults: 5, key: KEY }
+});
